714429667a1ed4dc3cccd5e4c6f666e46d1fcc94
Rae7771653 <nicole831220@gmail.com>
2022-08-14 01:09:03 +0800

    feat:add shorten URL function and modify show page

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express')
 const exhbs = require('express-handlebars')
-
+const shortenURL = require('./shortenURL.js')
 const app = express()
 const port = 3000
 
 app.engine('handlebars', exhbs({ defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   return res.render('show')
+})
+app.post('/', (req, res) => {
+  const inputURL = req.body.url
+  console.log(inputURL)
+  //輸入結果是非網址或空白
+  //輸入結果已重複輸入
+  const outputURL = shortenURL()
+  res.render('show', { outputURL })
 })
 
 app.listen(port, () => console.log(`It is listening on http://localhost:${port}`))
---
+++ shortenURL.js
@@ -0,0 +1,14 @@
+function shortenURL(){
+  let outputURL = 'http://localhost:3000/'//宣告最終產生的網址
+  const upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  const data = upperLetters + upperLetters.toLowerCase() + '1234567890'
+  const options = data.split('')//選項裡包括大寫字母、小寫字母和數字
+  //隨機從選項中抽取5個字母或數字
+  for(let i = 1; i <= 5; i++){
+    const index = Math.floor(Math.random() * options.length)
+    outputURL += options[index]
+  }
+  return outputURL
+}
+console.log(shortenURL())
+module.exports = shortenURL
