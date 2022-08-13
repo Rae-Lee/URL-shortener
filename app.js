5d9d531381ba863c58f153400bb7684c70f5531f
Rae7771653 <nicole831220@gmail.com>
2022-08-14 01:19:53 +0800

    feat:add copy function

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.get('/', (req, res) => {
 })
 app.post('/', (req, res) => {
   const inputURL = req.body.url
-  console.log(inputURL)
   //輸入結果是非網址或空白
   //輸入結果已重複輸入
   const outputURL = shortenURL()
